1145454d2dc7697b5e8b4a3f295375040a90c9b6
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2014-08-19 18:18:28 +0200

    Version 1.4.4

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "Account Voucher SEPA",
-    "version": "1.4.2",
+    "version": "1.4.4",
     "author": "XCG Consulting",
     "website": "http://www.openerp-experts.com",
     "category": 'Accounting',
