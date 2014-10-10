3b89da5698cb2c450393bc3d5ae49ba0a8e5c8f7
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-10-10 10:56:55 +0200

    Bumped to version 1.5.3 (stable)

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "Account Voucher SEPA",
-    "version": "1.5.2",
+    "version": "1.5.3",
     "author": "XCG Consulting",
     "website": "http://www.openerp-experts.com",
     "category": 'Accounting',
